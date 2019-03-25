27291ee03da1ba11f7df935c1c97a106e1424b25
renxb <rxb12321@gmail.com>
2019-03-25 14:13:18 +0800

    ma_op_rate_upstream is OK

--- pe/pe/spiders/chem99_ma_op_upstream.py
+++ pe/pe/spiders/chem99_ma_op_upstream.py
@@ -14,7 +14,7 @@ def filter_title(field):
 		return True
 
 class Chem99MaOpUpstreamWeek(SplashSpiderBase):
-	name = 'chem99_ma_op'
+	name = 'chem99_ma_op_up'
 
 	SEARCH_API_META = {
 		"keyword" : "国内甲醇装置有效开工率",
@@ -48,14 +48,11 @@ class Chem99MaOpUpstreamWeek(SplashSpiderBase):
 				
 				item['name'] = self.name
 				item['unit'] = '%'
-
-				news_info = self.visited_news.get( self._get_id_from_url(response.url) )
-				if news_info:
-					item['date'] = news_info['pubDate']
+				item['date'] = self.get_date_from_meta_info(response.url)
 
 				if i == 1: 		#全国开工率
 					item['filename'] = 'China_MA_operation_rate_weekly'
-				else:
+				else:		#西北开工率
 					item['filename'] = 'Nortwest_China_MA_operation_rate_weekly'
 
 				yield item
@@ -69,7 +66,7 @@ class Chem99MaOpUpstreamWeek(SplashSpiderBase):
 				item['productivity'] = self.clean_tags(row[2])
 				item['raw_material'] = self.clean_tags(row[3])
 				item['remarks'] = self.clean_tags(row[4])
-
+				item['unit'] =  u'万吨'
 				item['date'] = self.get_date_from_meta_info(response.url)
 				item['filename'] = 'Ma_operation_rate_detail_weekly'
 
@@ -80,52 +77,3 @@ class Chem99MaOpUpstreamWeek(SplashSpiderBase):
 			print e
 			print '-' * 50
 		print '=' * 40
-
-
-		# for tab_in_html in html_tabs:
-		# 	table = html_table_parsing(tab_in_html)
-
-		# 	for i in range(0,len(table)):
-		# 		# print table[i]
-		# 		self.print_row(table[i])
-
-		# 	print '----------------------------------------------'
-
-		# selector_templ = '#PanelContent > table > tbody > tr:nth-child({0}) > td:nth-child({1})'
-		# filename_map = {
-		# 	2 : 'MA_Jiangsu',
-		# 	3 : 'MA_Zhejiang',
-		# 	4 : 'MA_Guangdong',
-		# 	5 : 'MA_Fujian',
-		# }
-		# try:
-		# 	for i in range(2,6):
-		# 		item = Chem99MaOperationRateUpstreamWeek()
-				
-		# 		item['product_name'] = u'甲醇'
-		# 		item['inv'] = self._strip_html_tags( response.css(selector_templ.format(2,i)).extract_first() ).encode('utf-8')
-		# 		item['compare_to_last_week'] = self._strip_html_tags( response.css(selector_templ.format(3,i)).extract_first() ).encode('utf-8')
-		# 		item['change'] = self._strip_html_tags( response.css(selector_templ.format(4,i)).extract_first() ).encode('utf-8')
-		# 		item['unit'] = u'万吨'
-
-		# 		item['name'] = self.name
-		# 		item['filename'] = filename_map[i]
-
-		# 		print self.visited_news
-		# 		news_info = self.visited_news.get( self._get_id_from_url(response.url) )
-		# 		# print news_info
-		# 		if news_info:
-		# 			item['date'] = news_info['pubDate']
-				
-		# 		#item['to_update'] == None
-		# 		yield item
-		
-
-	def print_row(self, row):
-		LEN = len(row)
-
-		line = ''
-		for j in range(0, LEN):
-			line += self.clean_tags(row[j]) + ',  '
-
-		print line
