687c99786e7357cd0e6ee195c985d606b77df8b0
renxb <rxb12321@gmail.com>
2019-03-25 16:43:08 +0800

    MA_operation_rate_downstream is OK

--- pe/pe/spiders/chem99_ma_op_downstream.py
+++ pe/pe/spiders/chem99_ma_op_downstream.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 #化工-MA下游开工-周度
+import copy
 from splash_base import SplashSpiderBase
-
 from user_items import Chem99MaOperationRateDownstreamWeek
+from ..util import html_table_parsing
 
-def filter_sccid(field):
-	if field == '458':
+def filter_op_down(field):
+	if field == u'化工':
 		return False
 	else:
 		return True
@@ -21,52 +22,54 @@ class Chem99MaOpDownstreamWeek(SplashSpiderBase):
 	name = 'chem99_ma_op_down'
 
 	SEARCH_API_META = {
-		"keyword" : "国内甲醇装置有效开工率",
+		"keyword" : "甲醇下游周度开工率统计",
 		"sccid" : 0,
-		"filter" : [{
-			"field" : "SCCID",
-			"method" : filter_sccid
-		},{
-			"field" : "Title",
-			"method" : filter_title
-		}]
+		"filter" : {
+			"field" : "WebSite",
+			"method" : filter_op_down
+		}
 	}
 
 	LOGIN_TYPE = 'CHEM_LOGIN'
 
-	DEBUG_URL = 'http://chem.chem99.com/news/30417097.html'
+	# DEBUG_URL = 'http://chem.chem99.com/news/30417097.html'
 
 	def parse_page(self, response):
-		print '====================> parse chem99_ma_inv_week'
+		print '====================> parse chem99_ma_op_downstream_week'
 
-		selector_templ = '#PanelContent > table > tbody > tr:nth-child({0}) > td:nth-child({1})'
+		html_tab = response.css('#PanelContent table').extract_first()
+		table = html_table_parsing(html_tab)
+
 		filename_map = {
-			2 : 'MA_Jiangsu',
-			3 : 'MA_Zhejiang',
-			4 : 'MA_Guangdong',
-			5 : 'MA_Fujian',
+			1 : 'MTO_MTP',
+			2 : 'Jiaquan',	#甲醛
+			3 : 'Erjiami',	#二甲醚
+			4 : 'MTBE',
+			5 : 'Cusuan',	#醋酸
+			6 : 'Jiasuoquan',	#甲缩醛
+			7 : 'DMF',
+			8 : 'ALL'
 		}
+
 		try:
-			for i in range(2,6):
+			for i in range(1,8):
+
 				item = Chem99MaOperationRateDownstreamWeek()
-				
-				item['product_name'] = u'甲醇'
-				item['inv'] = self._strip_html_tags( response.css(selector_templ.format(2,i)).extract_first() ).encode('utf-8')
-				item['compare_to_last_week'] = self._strip_html_tags( response.css(selector_templ.format(3,i)).extract_first() ).encode('utf-8')
-				item['change'] = self._strip_html_tags( response.css(selector_templ.format(4,i)).extract_first() ).encode('utf-8')
-				item['unit'] = u'万吨'
+				item['product_name'] = self.clean_tags(table[0][i])
+				item['operation_rate'] = self.clean_tags(table[1][i])
+				item['operation_rate_last_week'] = self.clean_tags(table[2][i])
+				item['change_compare_to_last_week'] = self.clean_tags(table[3][i])
 
 				item['name'] = self.name
 				item['filename'] = filename_map[i]
+				item['date'] = self.get_date_from_meta_info(response.url)
 
-				print self.visited_news
-				news_info = self.visited_news.get( self._get_id_from_url(response.url) )
-				# print news_info
-				if news_info:
-					item['date'] = news_info['pubDate']
+
+				item_all = copy.deepcopy(item)
+				item_all['filename'] = filename_map[8]
 				
-				#item['to_update'] == None
 				yield item
+				yield item_all
 		except Exception as e:
 			print '-' * 50
 			print 'Error in parsing ' + response.url 
